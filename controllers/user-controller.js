d81b376008d8ce29b5fdeaa0b462671b3997ef06
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-07 12:39:49 +0530

    Updated user-controller.js
    Updated user-controller.js added all the nessary functions for get all the users, signup, login

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,6 +2,8 @@ const asyncWrapper = require('../middlewares/async')
 const { StatusCodes } = require('http-status-codes')
 const { NotFound, BadRequest, UnAuthorized } = require('../errors')
 
+const bycrypt = require('bcryptjs')
+
 const User = require('../models/User')
 
 const getAllUsers = asyncWrapper( async (req, res, next) => {
@@ -13,5 +15,41 @@ const getAllUsers = asyncWrapper( async (req, res, next) => {
     return res.status(StatusCodes.OK).json({users})
 })
 
+const signUp = asyncWrapper( async(req, res, next) => {
+    const { name, email, password } = req.body
+    const exsistingUser = await User.findOne({ email })
+    if(exsistingUser) {
+        throw new UnAuthorized(`User with email id : ${email} already exsists`, StatusCodes.UNAUTHORIZED)
+    }
 
-module.exports = getAllUsers
+    const hashedPassword = bycrypt.hashSync(password)
+    const user = new User({
+        name,
+        email,
+        password: hashedPassword,
+    })
+    await user.save()
+     res.status(StatusCodes.CREATED).json({user})
+})
+
+const login = asyncWrapper( async (req, res, next) => {
+    const { email, password } = req.body
+    const exsistingUser = await User.findOne({ email })
+    if(!exsistingUser) {
+        throw new NotFound(`User with email id : ${email} already exsists`, StatusCodes.NOT_FOUND)
+    }
+
+    const isPasswordCorrect = bycrypt.compareSync(password, exsistingUser.password)
+    if(!isPasswordCorrect) {
+        throw BadRequest('Incorrect password', StatusCodes.BAD_REQUEST)
+    }
+
+    res.status(StatusCodes.OK).json({message: "Logged in succesfully"})
+})
+
+
+module.exports = {
+    getAllUsers,
+    signUp,
+    login,
+}
