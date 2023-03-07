794d7d67b7a597d22462fc0822501190cc5c05ff
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-07 09:50:01 +0530

    Created index.js
    Created index.js and configured all other error codes into it

---
+++ errors/index.js
@@ -0,0 +1,12 @@
+const CustomError = require('./custom-error')
+const BadRequest = require('./bad-request')
+const UnAuthorized = require('./unauthorized')
+const NotFound = require('./not-found')
+
+
+module.exports = {
+    CustomError,
+    BadRequest,
+    UnAuthorized,
+    NotFound
+}
