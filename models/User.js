352d1f4abc3d49162a38c8b52d64f87121e76b95
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-07 09:25:30 +0530

    Created User.js in models directory
    Created User.js in models directory and added the user SChema

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter the name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide email']
+    },
+    password: {
+        trype: String,
+        required: [true, 'Please provide password'],
+        minlength: 3
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema)
