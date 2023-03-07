99ac0ca9d5ee6965075fffa2835fd4879a0b1d41
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-07 12:41:10 +0530

    Updated user0routes.js
    Updated user-routes.js added all the cntroller functions to route and configured it to use in app.js

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 
-const getAllUsers = require('../controllers/user-controller')
+const { getAllUsers, signUp, login } = require('../controllers/user-controller')
 
 const userRouter = express.Router()
 
+//userRouter.route('/').get(getAllUsers).post(signUp).post(login)
 userRouter.route('/').get(getAllUsers)
-
+userRouter.route('/signup').post(signUp)
+userRouter.route('/login').post(login)
 
 
 
