65b9aa35332c9a81238bc7c67313a5308c63ef4b
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-07 14:39:03 +0530

    Updated app.js
    Updated app.js and checked the functionality using postman

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const app = express()
 
 app.use(express.json())
 
+const connectDB = require('./db/connect')
+const userRouter = require('./routes/user-routes')
+const blogRouter = require('./routes/blog-routes')
+
+
 
 const port = process.env.PORT || 3000
 
+
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/blogs', blogRouter)
+
 const start = async () => {
     try {
-        // connect to DB
+        await connectDB(process.env.MONGO_URL)
         app.listen(port, console.log(`Server is listeniing on ${port}...`))
     } catch (error) {
         console.log(err)
