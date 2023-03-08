923d7bc8c0ee9edc6f1d49317026bbf2c5ccdbb2
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-08 10:18:27 +0530

    Updated blog-routes.js
    Added one more function

--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -2,11 +2,12 @@ const express = require('express')
 
 const blogRouter = express.Router()
 
-const { getAllBlogs, postBlog, updateBlog, getBlog, deleteBlog } = require('../controllers/blog-controller')
+const { getAllBlogs, postBlog, updateBlog, getBlog, deleteBlog, getBlogsOfUser } = require('../controllers/blog-controller')
 
 blogRouter.route('/').get(getAllBlogs)
 blogRouter.route('/post').post(postBlog)
 blogRouter.route('/update/:id').patch(updateBlog)
 blogRouter.route('/:id').get(getBlog).delete(deleteBlog)
+blogRouter.route('/user/:id').get(getBlogsOfUser)
 
 module.exports = blogRouter
